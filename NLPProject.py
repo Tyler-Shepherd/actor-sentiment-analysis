53e31d9027baca57ce04b02199fa0fe953b8de8d
Tyler Shepherd <tyleremail7@gmail.com>
2018-04-13 22:52:17 -0400

    implemented lexicon look up sentiment analysis

--- NLPProject.py
+++ NLPProject.py
@@ -1,6 +1,28 @@
 import csv
+from pprint import pprint
 
-if __name__ == "__main__":
+
+# Amartya's column numbers
+# has_actor_col = -1
+# review_col = 29
+# actor1_col = 30
+# actor1_sentiment_col = 31
+# actors_start_col = 32
+# actors_end_col = 0
+
+
+# Tyler's column numbers
+has_actor_col = 38
+review_col = 27
+actor1_col = 28
+actor1_sentiment_col = 29
+actors_start_col = 30
+actors_end_col = 2
+
+
+
+# if __name__ == "__main__":
+def get_review_data():
     counter = 0
     lis_movies = []
     unknown_str = "Unknown"
@@ -11,22 +33,23 @@ if __name__ == "__main__":
             counter += 1
             if counter == 1:
                 continue
-            if row[-1] == "No":
+            if row[has_actor_col] == "No":
                 continue
             current_dict = dict()
-            movie_review = row[29]
+
+            movie_review = row[review_col]
             current_dict["review"] = movie_review
-            info = row[30].split("|")
+            info = row[actor1_col].split("|")
             movie_name = info[0]
             if len(movie_name.replace(" ", "")) == 0:
                 movie_name = "Unknown" + str(counter)
             current_dict["title"] = movie_name
-            print movie_name
+            # print movie_name
             first_actor = info[1]
-            first_actor_review = row[31]
+            first_actor_review = row[actor1_sentiment_col]
             current_dict["actor_info"] = dict()
             current_dict["actor_info"][first_actor] = first_actor_review
-            for b in range(32, len(row) - 1, 2):
+            for b in range(actors_start_col, len(row) - 1 - actors_end_col, 2):
                 if row[b] == "{}":
                     break
                 current_dict["actor_info"][row[b]] = row[b + 1]
@@ -44,4 +67,5 @@ if __name__ == "__main__":
                     actor_dict[actor_name].append(sentence)
         lis_actor_sentences.append(actor_dict)
 
-    print lis_actor_sentences
+    return lis_movies, lis_actor_sentences
+    # pprint(lis_actor_sentences)
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,51 +1,151 @@
 from pprint import pprint
-
-lexicon = open('SentiWordNet/SentiWordNet.txt')
-
-term_to_data = {}
-
-for line in lexicon:
-	line = line.split('\t')
-
-	if line[0][0] == '#':
-		continue
-
-	pos = line[0]
-	ID = int(line[1])
-	pos_score = float(line[2])
-	neg_score = float(line[3])
-	definition = line[5]
-	definition = definition.rstrip()
-
-	terms = line[4]
-	words = []
-
-	while terms.find('#') != -1:
-		pound_spot = terms.find('#')
-
-		word = terms[:pound_spot]
-		sense_num = int(terms[pound_spot+1])
-
-		words.append((word, sense_num))
-		terms = terms[pound_spot+1:]
-		if terms.find(' ') != -1:
-			terms = terms[terms.find(' ')+1:]
-
-	for (term,sense_num) in words:
-		if term not in term_to_data:
-			term_to_data[term] = []
-
-		data = {}
-		data["sense_num"] = sense_num	
-		data["pos"] = pos
-		data["ID"] = ID
-		data["pos_score"] = pos_score
-		data["neg_score"] = neg_score
-		data["definition"] = definition
-
-		term_to_data[term].append(data)
-
-	
+import NLPProject
+import sys
 
 
-pprint(term_to_data)
+def read_lexicon():
+	lexicon = open('SentiWordNet/SentiWordNet.txt')
+
+	term_to_data = {}
+
+	for line in lexicon:
+		line = line.split('\t')
+
+		if line[0][0] == '#':
+			continue
+
+		pos = line[0]
+		ID = int(line[1])
+		pos_score = float(line[2])
+		neg_score = float(line[3])
+		definition = line[5]
+		definition = definition.rstrip()
+
+		terms = line[4]
+		words = []
+
+		while terms.find('#') != -1:
+			pound_spot = terms.find('#')
+
+			word = terms[:pound_spot]
+			sense_num = int(terms[pound_spot+1])
+
+			words.append((word, sense_num))
+			terms = terms[pound_spot+1:]
+			if terms.find(' ') != -1:
+				terms = terms[terms.find(' ')+1:]
+
+		for (term,sense_num) in words:
+			if term not in term_to_data:
+				term_to_data[term] = []
+
+			data = {}
+			data["sense_num"] = sense_num	
+			data["pos"] = pos
+			data["ID"] = ID
+			data["pos_score"] = pos_score
+			data["neg_score"] = neg_score
+			data["definition"] = definition
+
+			term_to_data[term].append(data)
+
+	return term_to_data
+
+
+def sentiment_analysis_using_lexicon(sentences, lexicon):
+	num_words_missing = 0
+	num_words = 0
+
+	score = 0
+
+	for sentence in sentences:
+		sentence = sentence.split()
+
+		# TODO: if we wanted to use the lexicon most efficiently we should do pos tagging on the sentences
+		# each word in the lexicon has multiple sentiments based on meaning and pos
+		# I'm just grabbing the first one for now
+
+		# TODO: remove punctuation (lots of "words" are just punctuation)
+
+		for word in sentence:
+			num_words += 1
+
+			if word not in lexicon:
+				num_words_missing += 1
+				continue
+
+			potential_word_sentiments = lexicon[word]
+
+			for word_sentiment in potential_word_sentiments:
+				if word_sentiment["sense_num"] != 1:
+					continue
+
+				pos_score = word_sentiment["pos_score"]
+				neg_score = word_sentiment["neg_score"]
+
+				score += (pos_score - neg_score)
+
+	return score, num_words_missing, num_words
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+	review_data, actor_sentences = NLPProject.get_review_data()
+
+	lexicon = read_lexicon()
+
+	total_num_actors = 0
+	num_actors_missing = 0
+	num_actors_retrieve_correct_sentiment = 0
+
+	total_num_words = 0
+	num_words_missing_from_lexicon = 0
+
+
+	for i in range(len(review_data)):
+		review = review_data[i]
+		actor_to_sentences = actor_sentences[i]
+
+		actor_info = review["actor_info"]
+
+		actor_scores = {}
+
+		for actor in actor_info.keys():
+			total_num_actors += 1
+
+			if actor not in actor_to_sentences:
+				num_actors_missing += 1
+				continue
+
+			review_sentences = actor_to_sentences[actor]
+
+			score, num_words_missing, num_words = sentiment_analysis_using_lexicon(review_sentences, lexicon)
+
+			num_words_missing_from_lexicon += num_words_missing
+			total_num_words += num_words
+
+			if score > 0 and actor_info[actor] == 'Positive':
+				num_actors_retrieve_correct_sentiment += 1
+			elif score < 0 and actor_info[actor] == 'Negative':
+				num_actors_retrieve_correct_sentiment += 1
+
+
+	print "num actors missing:", num_actors_missing
+	print "total actors:", total_num_actors
+
+	print "num words missing from lexicon:", num_words_missing_from_lexicon
+	print "total num words:", total_num_words
+
+	print "num actors retrieve correct sentiment:", num_actors_retrieve_correct_sentiment
+	print "error:", 1 - float(num_actors_retrieve_correct_sentiment) / total_num_actors
+
