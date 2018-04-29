cc79dada220240af4ca4a3c6382ee5d517a37ddb
AmartC <chakrab.amartya@gmail.com>
2018-04-29 17:26:58 -0400

    Added support for attention networks.

--- NLPProject.py
+++ NLPProject.py
@@ -5,11 +5,14 @@ import NameTagging
 import LexiconSentimentAnalysis
 import NLPParsing
 import ReadDNNResults
+import os
+from subprocess import call
 
 sys.path.insert(0, './lib/DNN-Sentiment')
 
 import getSentiment
-
+sys.path.insert(0, './lib/LSTM/reviews')
+import actorReview
 
 if __name__ == "__main__":
     if len(sys.argv) <= 2:
@@ -21,6 +24,7 @@ if __name__ == "__main__":
     algo_type = sys.argv[2]
 
     review_file = open("./data/examples/"+review_filename, "r")
+    output_file = open("./lib/LSTM/data/actors/new_review.txt", "w")
 
     review = ""
 
@@ -69,8 +73,40 @@ if __name__ == "__main__":
     for entity in named_entities:
         if algo_type == "-l":
             sentiment = LexiconSentimentAnalysis.GetSentiment(entity, review)
+            print(entity, sentiment, flush=True)
         elif algo_type == "-r" or algo_type == "-c":
             actor_sentences = NLPParsing.get_actor_sentences(entity, review)
             sentiment = ReadDNNResults.GetSentiment("./lib/DNN-Sentiment/data/actor_sentences.csv", actor_sentences)
+            print(entity, sentiment, flush=True)
+        elif algo_type == "-a":
+            rev_line = review.replace(entity,"$T$")
+            rev_line = rev_line.replace(entity.lower(), "$T$")
 
-        print(entity, sentiment, flush=True)
+            # Replace all mentions of just one word in the actors name (i.e. their last name)
+            for word in entity.split():
+                if len(word) < 2:
+                    # Ignore single letters (like an initial)
+                    continue
+                rev_line = rev_line.replace(word, "$T$")
+                rev_line = rev_line.replace(word, "$T$")
+
+            while "$T$ $T$" in rev_line:
+                rev_line = rev_line.replace("$T$ $T$", "$T$")
+            rev_line += '\n'
+
+            aspect_line = entity + "\n"
+            sentiment_line = "0"
+            output_file.write(rev_line + aspect_line + sentiment_line +"\n")
+    if algo_type == "-a":
+        positive = "$T$ did a phenomenal job in black panther which overall was a solid movie but a little bit overhyped. the rest of the cast was great but i was mostly excited to watch him.\n"
+        positive += "chadwick boseman\n" + "1\n"
+        output_file.write(positive)
+        neutral = "$T$ did a mediocre job in black panther which overall was a solid movie but a little bit overhyped. the rest of the cast was great but i was mostly excited to watch him.\n"
+        neutral += "mark jones\n" +  "0\n"
+        output_file.write(neutral)
+        terrible = "$T$ did a horrible job in black panther which overall was a solid movie but a little bit overhyped. the rest of the cast was great but i was mostly excited to watch him.\n"
+        terrible += "johnny adams\n" + "-1\n"
+        output_file.write(terrible)
+    output_file.close()
+    os.chdir("lib/LSTM/reviews")
+    call(["py", "-3", "actorReview.py"])
